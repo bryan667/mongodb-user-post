23da04c457b38e5c33a29040b591cc9b4525d5c2
Bryan M <janbryanmartirez@gmail.com>
2019-01-24 14:10:13 +0800

    wow

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware')
-// const port = process.env.PORT || 3010
+const port = process.env.PORT || 3010
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: `https://evening-waters-88095.herokuapp.com:3010/` }))
+    app.use(proxy('/api', { target: `https://evening-waters-88095.herokuapp.com:${port}` }))
 }
